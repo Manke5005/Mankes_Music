7708ed5b9c0f58ad6f9d490f3941dcd24aa5862d
Manke <zane.mason1552@gmail.com>
2024-09-06 09:20:49 -0700

    Initial commit

---
+++ src/main/java/net/zane/mankesmusic/MankesMusic.java
@@ -0,0 +1,16 @@
+package net.zane.mankesmusic;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MankesMusic implements ModInitializer {
+	public static final String MOD_ID = "mankesmusic";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
